b3f52ac7a9a79026c5b289e3a23de63a0c7ab4a7
anastasiaayesa-cloud <anastasiaayesa@gmail.com>
2025-12-12 10:43:03 +0700

    Mengganti pangkat menjadi select dan menampilkan data pangkat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            KabupatenSeeder::class
+            KabupatenSeeder::class,
+            PangkatSeeder::class
         ]);
     }
 }
