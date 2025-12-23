a261231a22a85770d75fa6d248c93055fc7589a8
Eka Bulandary <ekbuldy18@gmail.com>
2025-12-23 09:57:49 +0700

    commit

--- app/Models/Rencana.php
+++ app/Models/Rencana.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Rencana extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nama_kegiatan',
+        'tanggal_kegiatan',
+        'pegawai',
+    ];
 }
